cb2c59938fac15e3c7f2fbdd22d5d7665613e787
Kononovich Halina <kononovich1703@gmail.com>
2020-06-15 21:08:16 +0300

    f.prototype

--- script.js
+++ script.js
@@ -191,3 +191,26 @@ function SC(name, floors, shopsOnFloor) {
 }
 
 const shop1 = new SC('shop1', 3, 20);
+
+/*
+Создать класс “Мебель” с базовыми свойствами “имя”, “цена” и методом “получить информацию” (метод должен вывести имя и цену). Метод должен быть объявлен с помощью прототипов (Func.prototype...). Создать два экземпляра класса “Мебель”: экземпляр “ОфиснаяМебель” и
+“Мебель для дома”. Придумайте им по одному свойству, которые будут характерны только для этих экземпляров (например, для офисной мебели - наличие компьютерного стола или шредера). Метод “получить информацию” должен учитывать и добавленное вами новое свойство.
+Задача на переопределение метода у экземпляров класса.
+*/
+
+function Furniture(name, price) {
+    this.name = name;
+    this.price = price;
+}
+
+Furniture.prototype.getInfo = function() {
+    return `${this.name} ${this.price}`;
+}
+
+const houseFurniture = new Furniture('chair', 200);
+houseFurniture.color = 'white';
+houseFurniture.getInfo = `${houseFurniture.getInfo()}  ${houseFurniture.color}`;
+
+const officeFurniture = new Furniture('printer', 500);
+officeFurniture.color = 'black';
+officeFurniture.getInfo = `${officeFurniture.getInfo()} ${officeFurniture.color} `
